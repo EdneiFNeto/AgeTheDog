9ffdbdbe43ffac6f4408c47650cc89ea8632ae9f
EdneiFreitas <edneifneto@gmail.com>
2022-05-21 10:05:53 -0300

    Create function age dog

--- IdadeDeCachorro/ViewController.swift
+++ IdadeDeCachorro/ViewController.swift
@@ -1,19 +1,18 @@
-//
-//  ViewController.swift
-//  IdadeDeCachorro
-//
-//  Created by Ednei Neto on 21/05/22.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldAgeDog: UITextField!
+    @IBOutlet weak var labelAgeDog: UILabel!
+    
+    @IBAction func showAgeDog(_ sender: Any) {
+        let ageDog = Int(textFieldAgeDog.text!)! * 7
+        labelAgeDog.text = "A idade do cachorro é " + String(ageDog)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
